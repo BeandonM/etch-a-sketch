d859f2b8549b2a6371790bcf66348dd55e877d35
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2024-01-14 13:47:01 -0600

    Added ability to change grid size Added ability to paint cell colors with mouse functions Added clear grid function Fixed text to update with the change grid size slider

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
-const DEFAULT_COLOR = '#00000';
+const DEFAULT_COLOR = '#000000';
 const DEFAULT_MODE = 'color'
 const DEFAULT_GRIDSIZE = 16;
 
 const GRID = document.querySelector('.grid-container');
+const GRID_SIZE_SLIDER = document.getElementById('size-slider');
+const GRID_TEXT = document.querySelector('.selected-size');
 
+let currentColor = DEFAULT_COLOR;
+let gridSize = DEFAULT_GRIDSIZE;
+let mouseDown = false;
+function setGridSizeText()
+{
+    GRID_TEXT.textContent = `${GRID_SIZE_SLIDER.value}`;
+    gridSize = GRID_SIZE_SLIDER.value;
+}
+
+function updateGridSize()
+{
+    clearGrid();
+    createGrid(gridSize);
+}
+
+function clearGrid()
+{
+    document.querySelectorAll('.cell').forEach(cell => {
+        cell.style.backgroundColor = 'white';
+    })
+}
+function createListeners()
+{
+    GRID_SIZE_SLIDER.addEventListener("input",setGridSizeText,false);
+    GRID_SIZE_SLIDER.addEventListener("change",updateGridSize,false);
+    document.body.onmousedown = () => (mouseDown = true);
+    document.body.onmouseup = () => (mouseDown = false);
+}
+function setColor()
+{
+
+}
+function setCellColor(e)
+{
+    if(e.type === 'mouseover' && !mouseDown)
+    {
+        return;
+    }
+    e.target.style.backgroundColor = currentColor;
+}
 function createGrid(size)
 {
-    GRID.textContent = '';
+    GRID.textContent = ''
 
     for(let i = 0; i < size; i++)
     {
@@ -14,6 +56,8 @@ function createGrid(size)
         {
             const cell = document.createElement('div');
             cell.classList.add('cell');
+            cell.addEventListener('mousedown',setCellColor);
+            cell.addEventListener('mouseover',setCellColor);
             let scale = 100/size;
             cell.style.width = `${scale}%`
             cell.style.height = `${scale}%`
@@ -25,8 +69,8 @@ function createGrid(size)
     div.classList.add('cell');
     GRID.appendChild(div);
 }
-
-createGrid(16);
+createListeners();
+createGrid(gridSize);
 /*
 const GRID_CONTAINER = document.querySelector('.grid-container');
 const cell = document.createElement('div');
